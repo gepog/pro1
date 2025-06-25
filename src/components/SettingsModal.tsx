8c2c30c7cd2904bfeffc98cba40d965ca9fbf782
gepog <gepoguo@gmail.com>
2025-06-25 14:17:00 +0200

    Updated SettingsModal.tsx

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -248,6 +248,7 @@ export const SettingsModal: React.FC<SettingsModalProps> = ({ isOpen, onClose })
                       <div className="w-11 h-6 bg-gray-600 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all" style={{ '--peer-checked-bg': '#ddb870' }}></div>
                     </label>
                   </div>
+                  </div>
                 </div>
               </div>
             )}
